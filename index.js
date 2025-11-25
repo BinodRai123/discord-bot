6484324f224f9dc0c5b1fbee8ecbf733c3b6282f
binod rai <br163406@gmail.com>
2025-11-25 22:57:14 +0545

    checking if the message author is bot or not , if not then replying them with message

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({intents: [GatewayIntentBits]});
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 client.once("ready", () => {
-    console.log("bot is ready");
-})
+  console.log("bot is ready");
+});
+
+client.on("messageCreate", (message) => {
+  // console.log(`${message.author.username} send message -> ${message.content}`);
+  const bot = message.author.bot;
+  if (bot) return;
+
+    message.reply("hello how can i help you today ?");
+  
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
