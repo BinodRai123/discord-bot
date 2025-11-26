78ca189e1753855ecff182d642e357603742b11d
binod rai <br163406@gmail.com>
2025-11-26 14:28:10 +0545

    initialized gemini image generator

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
+const { GoogleGenAI } = require("@google/genai"); 
+const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
+
+const  ImageGenerator = async () => {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash-image",
+    prompt: "generate a banana with a ice-cream on it"
+  })
+
+  for (const part of response.candidates[0].content.parts) {
+    if (part.text) {
+      console.log(part.text);
+    } else if (part.inlineData) {
+      const imageData = part.inlineData.data;
+      const buffer = Buffer.from(imageData, "base64");
+      fs.writeFileSync("gemini-native-image.png", buffer);
+      console.log("Image saved as gemini-native-image.png");
+    }
+  }
+}
 
 const client = new Client({
   intents: [
@@ -14,15 +34,14 @@ client.once("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-  // console.log(`${message.author.username} send message -> ${message.content}`);
+  console.log(message.attachments);
   const bot = message.author.bot;
-  if (bot) {
-    message.reply("Don't mimic me madarchod bot");
-    return;
-  }
+  if (bot) return;
 
     message.reply("hello how can i help you today ?");
   
 });
+
+
 
 client.login(process.env.DISCORD_BOT_TOKEN);
